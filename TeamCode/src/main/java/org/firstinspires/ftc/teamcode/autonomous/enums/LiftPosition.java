ae12243e292f746b5e90da02a1dcb0372320512e
Todd Schelling <tschelling@apple.com>
2022-11-30 00:21:23 -0600

    Made enmus match Mode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/enums/LiftPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/enums/LiftPosition.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode.autonomous.enums;
 
 public enum LiftPosition {
-    HIGH, MED, LOW, JUNC, REST, CONE_5, CONE_4, CONE_3, CONE_2
+    HIGH, MED, LOW, JUNC, REST, CONE_5, CONE_4, CONE_3, CONE_2, INIT
 }
